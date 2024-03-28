3deebc989f6d687cb356cf2c4b3a5b70446ebfbc
chkang13 <34392347+chkang13@users.noreply.github.com>
2024-03-28 09:28:23 +0900

    [Silver III] Title: 파일 정리, Time: 2148 ms, Memory: 41784 KB -BaekjoonHub

---
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+N = int(input())
+save = defaultdict(int)
+
+while N>0:
+    name,ext = input().split('.')
+    save[ext] += 1
+    N -= 1
+
+exts = list(save.keys())
+exts.sort()
+
+for ext in exts:
+    print(f'{ext} {save[ext]}')
