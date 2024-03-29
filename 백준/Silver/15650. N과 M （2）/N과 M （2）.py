c0c1fc6bc8a253f9f443a1d73771573b6301c868
chkang13 <34392347+chkang13@users.noreply.github.com>
2024-03-29 20:28:20 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,8 +1,21 @@
-from itertools import combinations
+import sys
+input = sys.stdin.readline
 
 N,M = map(int, input().split())
-nums = [i for i in range(1,N+1)]
-perm = list(combinations(nums,M))
 
-for i in perm:
-    print(*i)
+visited = [False] * N
+answer = []
+
+def back_track(index, N, M,s):
+    if index == M:
+        print(*answer)
+        return
+    for i in range(s,N):
+        if not visited[i]:
+            visited[i] =True
+            answer.append(i+1)
+            back_track(index+1, N, M, i+1)
+            visited[i] = False
+            answer.pop()
+
+back_track(0,N,M,0)
