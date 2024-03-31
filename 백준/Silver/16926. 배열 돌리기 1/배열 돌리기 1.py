14f661f46f4490c69b279e3bf9ea39abd7927473
chkang13 <34392347+chkang13@users.noreply.github.com>
2024-04-01 00:56:40 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 756 ms, Memory: 112380 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
@@ -1,36 +1,38 @@
-n, m, r = map(int, input().split(' '))
-graph = [list(map(int, input().split(' '))) for _ in range(n)]
+# 제한조건에 따라 사각형을 돌지 않는 경우가 없음
+# 시간 초과
+import sys
+input = sys.stdin.readline
+
+n,m,r = map(int, input().split())
+
+arr = [list(map(int, input().split())) for _ in range(n)]
 
 for _ in range(r):
-    for i in range(min(n, m) // 2):
-        s_x, s_y = i, i
-        s_value = graph[s_x][s_y]
-
-        for j in range(i + 1, n - i):  #좌
-            s_x = j
-            prev_value = graph[s_x][s_y]
-            graph[s_x][s_y] = s_value
-            s_value = prev_value
-
-        for j in range(i + 1, m - i):  #하
-            s_y = j
-            prev_value = graph[s_x][s_y]
-            graph[s_x][s_y] = s_value
-            s_value = prev_value
-
-        for j in range(i + 1, n - i):  #우
-            s_x = n - j - 1
-            prev_value = graph[s_x][s_y]
-            graph[s_x][s_y] = s_value
-            s_value = prev_value
-
-        for j in range(i + 1, m - i):  #상
-            s_y = m - j -1
-            prev_value = graph[s_x][s_y]
-            graph[s_x][s_y] = s_value
-            s_value = prev_value
+    # 돌아가는 사각형 갯수
+    for i in range(min(n,m)//2):
+        start = arr[i][i]
+        # 하로 이동
+        for j in range(i+1, n-i):
+            temp = arr[j][i]
+            arr[j][i] = start
+            start = temp
+        # 우로 이동
+        for j in range(i+1,m-i):
+            temp = arr[n-i-1][j]
+            arr[n-i-1][j] = start
+            start = temp
+        # 상으로 이동
+        for j in range(i+1, n-i):
+            temp = arr[n-j-1][m-i-1]
+            arr[n-j-1][m-i-1] = start
+            start = temp
+        # 좌로 이동
+        for j in range(i+1, m-i):
+            temp = arr[i][m-j-1]
+            arr[i][m-j-1] = start
+            start = temp
 
 for i in range(n):
     for j in range(m):
-        print(graph[i][j], end=' ')
+        print(arr[i][j], end=' ')
     print()
