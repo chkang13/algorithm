0d86554db70e0a16ffaf2e5d0d680c6f179ae9bd
chkang13 <34392347+chkang13@users.noreply.github.com>
2024-04-04 15:07:09 +0900

    [Silver II] Title: N번째 큰 수, Time: 1024 ms, Memory: 33188 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+from heapq import *
+
+n = int(input())
+
+heap = []
+heapify(heap)
+for i in range(n):
+    # 첫 번째 행의 값들 입력
+    table = list(map(int, input().split()))
+    for j in range(n):
+        if len(heap) < n: # 최소힙의 크기 제한
+            heappush(heap, table[j])
+
+        else:
+            if heap[0] < table[j]: # 최솟값보다 현재값이 큰 경우
+                heappop(heap)
+                heappush(heap, table[j])
+
+print(heap[0])
