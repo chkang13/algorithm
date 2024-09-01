d37e8699f5e600e0b5864148b803f09205278549
chkang13 <34392347+chkang13@users.noreply.github.com>
2024-09-01 23:39:53 +0900

    [Bronze II] Title: 문자열 반복, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,9 +1,9 @@
 T = int(input())
-P = [""]*T
-for i in range(T):
+P = [""] * T
+for i in range(T) : 
     R,S = input().split()
-    for word in S:
-        P[i] += word*int(R)
+    for word in S :
+        P[i] += word * int(R)
 
 for p in P:
     print(p)
